3a10b3ef11d73e3c637d61fb38ef225717ffba02
Farzad Qasim <qasimf@tcd.ie>
2016-05-22 18:24:24 +0100

    [SETUP] Added React

--- app/index.js
+++ app/index.js
@@ -1,1 +1,12 @@
-console.log('babel, eslint, webpack');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class WalkingSkeleton extends Component {
+  render() {
+    return (
+      <div>Hello, I'm a Walking Skeleton </div>
+    );
+  }
+}
+
+ReactDOM.render(<WalkingSkeleton />, document.getElementById('app'));
