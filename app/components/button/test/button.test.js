c89d93ce1b17997bc7627c065c8de7fa5f0d945b
Farzad Qasim <qasimf@tcd.ie>
2016-05-23 13:44:32 +0100

    [SETUP] Added Enzyme for testing

--- app/components/button/test/button.test.js
+++ app/components/button/test/button.test.js
@@ -1,7 +1,18 @@
 import expect from 'expect';
+import React from 'react';
+import {shallow} from 'enzyme';
+import Button from '../index.js'
 
-describe('Walking Skeleton test', () => {
-    it('should pass', () => {
-        expect(true).toEqual(true);
+function setup() {
+    const props = {
+        actions: {}
+    };
+    return shallow(<Button {...props}/>);
+}
+
+describe('Button test', () => {
+    it('should have a button element', () => {
+        const wrapper = setup();
+        expect(wrapper.find('button').length).toBe(1);
     });
 });
